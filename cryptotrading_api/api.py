4531b2964fb1c1604bca815b22d956f986207d68
Santeri Moberg <santeri.moberg30@gmail.com>
2019-04-04 15:22:32 +0300

    Revert "repon structure muokattu tän mukaiseksi kurssin ohjeiden mukaiseksi"
    This reverts commit a18c2912777f2b090d95b9fd7322861aba5695d8.

--- cryptotrading_api/api.py
+++
@@ -1,7 +0,0 @@
-from Cryptotrading.db import database
-from flask import Flask, Response, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import IntegrityError, DataError
-from flask_restful import Resource, Api
-from jsonschema import validate, ValidationError
-import json
--- src/database.py
+++ src/database.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import click
-from flask.cli import with_appcontext
-from cryptotrading_api import db
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 class Orders(db.Model):
     API_key = db.Column(db.String(24), nullable=False)
@@ -11,8 +12,3 @@ class Orders(db.Model):
     Order_size = db.Column(db.Integer, nullable=False)
     Order_side = db.Column(db.String(4), nullable=False)
     Order_symbol = db.Column(db.String(10), nullable=False)
-
-@click.command("init-db")
-@with_appcontext
-def init_db_command():
-    db.create_all()

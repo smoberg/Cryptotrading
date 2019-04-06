613c2152e2f9082eaab972be5135b2cb6a3a77e1
Santeri Moberg <santeri.moberg30@gmail.com>
2019-04-06 13:24:51 +0300

    masoncontrols

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError, DataError
 from flask_restful import Resource, Api
 from jsonschema import validate, ValidationError
+from utils import MasonBuilder
 import json
 
 app = Flask(__name__)
@@ -10,3 +11,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 api = Api(app)
+
+class MasonControls(MasonBuilder):
+    # attaches mason hypermedia controls to response bodies
+    def add_control_asd(self):
+        pass

d24c300435c631995b0ee344154815a5426e07b4
Santeri Moberg <santeri.moberg30@gmail.com>
2019-04-06 14:34:19 +0300

    Resourcet lisätty

--- src/app.py
+++ src/app.py
@@ -6,6 +6,8 @@ from jsonschema import validate, ValidationError
 from utils import MasonBuilder
 import json
 
+MASON = "application/vnd.mason+json"
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -16,3 +18,58 @@ class MasonControls(MasonBuilder):
     # attaches mason hypermedia controls to response bodies
     def add_control_asd(self):
         pass
+
+@app.route("/", methods=["GET"])
+def entrypoint():
+    body = MasonControls()
+    return Response(json.dumps(body), status=200, mimetype=MASON)
+
+class AccountInformation(Resource):
+    def get(self):
+        return Response(status=503)
+
+class AccountBalance(Resource):
+    def get(self):
+        return Response(status=503)
+class TransactionHistory(Resource):
+    def get(self):
+        return Response(status=503)
+
+class OrdersResource(Resource):
+    def get(self):
+        return Response(status=503)
+
+class OrderResource(Resource):
+    def get(self):
+        return Response(status=503)
+
+class OrderHistory(Resource):
+    def get(self):
+        return Response(status=503)
+
+class OrderBook(Resource):
+    def get(self):
+        return Response(status=503)
+
+class PriceAction(Resource):
+    def get(self):
+        return Response(status=503)
+
+class BucketedPriceAction(Resource):
+    def get(self):
+        return Response(status=503)
+
+class Positions(Resource):
+    def get(self):
+        return Response(status=503)
+
+class Position(Resource):
+    def get(self):
+        return Response(status=503)
+
+def create_error_response(status_code, title, message=None):
+    resource_url = request.path
+    body = MasonBuilder(resource_url=resource_url)
+    body.add_error(title, message)
+    body.add_control("profile", href=ERROR_PROFILE)
+    return Response(json.dumps(body), status_code, mimetype=MASON)

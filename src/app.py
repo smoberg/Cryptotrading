f434156ab7c89daf6c957d8781067bb95c46c09d
Santeri Moberg <santeri.moberg30@gmail.com>
2019-04-07 17:46:34 +0300

    Account resource lisätty, authorize funktio lisätty Accountiin implementoitu post metodi, yksittäisiin accounteihin implementoitu get ja delete sekä lisätty hypermedia controllit

--- src/app.py
+++ src/app.py
@@ -17,13 +17,35 @@ api = Api(app)
 
 class MasonControls(MasonBuilder):
     # attaches mason hypermedia controls to response bodies
-    def add_control_account(self):
-        self.add_control("account", href=api.url_for(AccountInformation),
+    @staticmethod
+    def account_schema():
+        schema = {
+            "type": "object",
+            "required": ["accountname", "api_public", "api_secret"]
+        }
+        props = schema["properties"] = {}
+        props["accountname"] = {
+            "description": "name of the account",
+            "type": "string"
+        }
+        props["api_public"] = {
+            "description": "public part of the api-key",
+            "type": "string"
+        }
+
+        props["api_secret"] = {
+            "description": "secret part of the api-key",
+            "type": "string"
+        }
+        return schema
+
+    def add_control_account(self, apikey):
+        self.add_control("account", href=api.url_for(AccountInformation, apikey=apikey),
                         method="GET",
                         title="Get general account information")
 
-    def add_control_orders(self):
-        self.add_control("orders", href=api.url_for(OrdersResource),
+    def add_control_orders(self, apikey):
+        self.add_control("orders", href=api.url_for(OrdersResource, apikey=apikey),
                         method="GET",
                         title="Get open orders")
 
@@ -37,47 +59,106 @@ class MasonControls(MasonBuilder):
                         method="GET",
                         title="Show recent trades that happened in the market")
 
-    def add_control_positions(self):
-        self.add_control("positions", href=api.url_for(Positions),
+    def add_control_positions(self, apikey):
+        self.add_control("positions", href=api.url_for(Positions, apikey=apikey),
                         method="GET",
                         title="Get open positions")
 
-    def add_control_accountbalance(self):
-        self.add_control("balance", href=api.url_for(AccountBalance),
+    def add_control_accountbalance(self, apikey):
+        self.add_control("balance", href=api.url_for(AccountBalance, apikey=apikey),
                         method="GET",
                         title="Get account balance")
 
-    def add_control_transactionhistory(self):
-        self.add_control("transactions", href=api.url_for(TransactionHistory),
+    def add_control_transactionhistory(self, apikey):
+        self.add_control("transactions", href=api.url_for(TransactionHistory, apikey=apikey),
                         method="GET",
                         title="Get history of the wallet transactions")
+
+    def add_control_add_account(self):
+        self.add_control("add-account", href=api.url_for(Account),
+                        method="POST",
+                        encoding="json",
+                        title="add account to cryptotrading api",
+                        schema=self.account_schema())
+
+    def add_control_delete_account(self, apikey):
+        self.add_control("delete", href=api.url_for(AccountInformation, apikey=apikey),
+                        method="DELETE",
+                        title="delete this account")
 
 
 @app.route("/", methods=["GET"])
 def entrypoint():
     body = MasonControls()
-    body.add_control_account()
+    body.add_control_add_account()
     body.add_control_orders()
     body.add_control_orderbook()
     body.add_control_priceaction()
     body.add_control_positions()
     return Response(json.dumps(body), status=200, mimetype=MASON)
 
-class AccountInformation(Resource):
+class Account(Resource):
     def get(self):
+        # add control to entrypoint maybe
+        body = MasonControls()
+        body.add_control_add_account()
+        return Response(json.dumps(body), status=200, mimetype=MASON)
+
+    def post(self):
+        if not request.json:
+            return create_error_response(415, "Unsupported media type", "Requests must be JSON")
+        try:
+            validate(request.json, MasonControls.account_schema())
+        except ValidationError as e:
+            return create_error_response(400, "Invalid JSON document", str(e))
+
+        user = User(username=request.json["accountname"], api_public=request.json["api_public"],
+                        api_secret=request.json["api_secret"])
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+        except IntegrityError:
+            return create_error_response(409, "Already exists",
+                                        "Account with name '{}' already exists.".format(request.json["accountname"]))
+        return Response(status=201, headers={"Location": api.url_for(AccountInformation, apikey=request.json["api_public"])})
+
+class AccountInformation(Resource):
+    def get(self, apikey):
         # sends request to bitmex websocket api, retrieves Response
         # Parses response
         # Adds json controls to body
         # Return response
         # pitääköhä olla controlli takasin entrypointtii?
+        acc = User.query.filter_by(api_public=apikey).first()
+        authorize(acc, request)
+        body = MasonControls(accountname=acc.username, api_public=acc.api_public, api_secret=acc.api_secret)
+        body.add_control("self", api.url_for(AccountInformation, apikey=apikey))
+        body.add_control_orders(apikey)
+        body.add_control_accountbalance(apikey)
+        body.add_control_positions(apikey)
+        body.add_control_transactionhistory(apikey)
+        body.add_control_delete_account(apikey)
+        return Response(json.dumps(body), status=200, mimetype=MASON)
 
-        body = MasonControls()
-        body.add_control_orders()
-        body.add_control_accountbalance()
-        body.add_control_positions()
-        body.add_control_transactionhistory()
+    def delete(self, apikey):
+        acc = User.query.filter_by(api_public=apikey).first()
+        if not acc:
+            return create_error_response(404, "Account does not exist", "Account with api-key '{}' does not exist.".format(apikey))
+        authorize(acc, request)
+        db.session.delete(acc)
+        db.session.commit()
+        return Response(status=204)
 
-        return Response(body, status=200, mimetype=MASON)
+
+def authorize(model, request):
+    # takes in user model and request object
+    # compares request object's api key to the saved api key in the database
+    if not request.headers["api_secret"]:
+        return create_error_response(401, "Unauthorized", "need secret api-key in the http header")
+    if request.headers["api_secret"] != model.api_secret:
+        return create_error_response(401, "Unauthorized", "Wrong api-key")
+    pass
 
 class AccountBalance(Resource):
     def get(self):
@@ -129,8 +210,8 @@ class Position(Resource):
     def get(self):
         return Response(status=503)
 
-
-api.add_resource(AccountInformation,"/account/")
+api.add_resource(Account,"/account/")
+api.add_resource(AccountInformation,"/account/<apikey>")
 api.add_resource(OrdersResource,"/orders/")
 api.add_resource(PriceAction, "/priceaction/")
 api.add_resource(Positions, "/positions/")
@@ -142,5 +223,5 @@ def create_error_response(status_code, title, message=None):
     resource_url = request.path
     body = MasonBuilder(resource_url=resource_url)
     body.add_error(title, message)
-    body.add_control("profile", href=ERROR_PROFILE)
+    # body.add_control("profile", href=ERROR_PROFILE)
     return Response(json.dumps(body), status_code, mimetype=MASON)

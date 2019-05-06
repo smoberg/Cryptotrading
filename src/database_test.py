1022791bd1602cef6b11499d53772d821c02cdfb
Santeri Moberg <santeri.moberg30@gmail.com>
2019-05-06 21:26:00 +0300

    problem with app context

--- src/database_test.py
+++ src/database_test.py
@@ -1,56 +1,68 @@
-from database import db, Orders, User
+import pytest
+import tempfile
+import os
+import app
+from database import User, Orders, db
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+from sqlalchemy.exc import IntegrityError, StatementError
 
-""" tää pitää korjata joskus"""
+"""
+These tests are based on the database testing example found in lovelace
 
-def main():
-    db.create_all()
+"""
 
-    #add order to database
-    user_1 = User(username="mikkomallikas", api_public="79z47uUikMoPe2eADqfJzRBu",
-                    api_secret="j9ey6Lk2xR6V-qJRfN-HqD2nfOGme0FnBddp1cxqK6k8Gbjd")
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
-    order = Orders(order_id='00000000-0000-0000-0000-000000000000',
-                    order_size=1, order_side='Buy',
-                    order_symbol="XBTUSD", user=user_1)
+@pytest.fixture
+def db_handle():
+    db_fd, db_fname = tempfile.mkstemp()
+    app.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_fname
+    app.app.config["TESTING"] = True
+    db.init_app(app.app)
 
-    db.session.add(user_1)
-    db.session.add(order)
-    db.session.commit()
+    with app.app.app_context():
+        db.create_all()
 
-    user = User.query.first()
-    order = Orders(order_id='00000000-0000-0000-0000-000dsd000000',
-                    order_size=1123, order_side='sell',
-                    order_symbol="XBTUSD", user=user)
+    yield db
 
-    db.session.add(order)
-    db.session.commit()
+    db.session.remove()
+    os.close(db_fd)
+    os.unlink(db_fname)
 
-    user = User.query.first()
-    for order in user.orders:
-        print(order.order_id)
+def _get_user(number=1):
+    return User(username="testuser-{}".format(number),
+                api_public="79z47uUikMoPe2eADqfJzRB{}".format(number),
+                api_secret="j9ey6Lk2xR6V-qJRfN-HqD2nfOGme0FnBddp1cxqK6k8Gbj{}".format(number))
 
+def _get_order(number=1):
+    return Orders(order_id='00000000-0000-0000-0000-00000000000{}'.format(number),
+                  order_size=1, order_side='Buy',
+                  order_symbol="XBTUSD")
 
-
-
+def test_create_instances(db_handle):
     """
-    #read database, and print order size
-    order1 = Orders.query.first()
-    print("Order size of the queried object before the modification is %d\n" % order1.Order_size)
-
-    #modify order size
-    modify(order1)
-    #print order size again
-    order1 = Orders.query.first()
-    print("Order size of the queried object after the modification is %d\n" % order1.Order_size)
-
-    ordertest = Orders.query.first()
-    delete(ordertest)
-
-    #Deletion, print is None which means that the database is empty
-    ordertest = Orders.query.first()
-    print("Print of the queried object:")
-    print(ordertest)
-    print("It is None because the database is empty after deletion of the single model")
+    Tests that we can create one instance of each model and save them to the
+    database using valid values for all columns. After creation, test that
+    everything can be found from database, and that all relationships have been
+    saved correctly.
     """
-if __name__ == '__main__':
-    main()
+    # Make everything, add order to user's order relationship
+    user = _get_user()
+    order = _get_order()
+    user.orders.append(order)
+    db_handle.session.add(user)
+    db_handle.session.add(order)
+    db.session.commit()
+    # Check that everything exists
+    assert User.query.count() == 1
+    assert Orders.query.count() == 1
+    db_user = User.query.first()
+    db_order = Orders.query.first()
+    # Relationship check
+    assert db_order in db_user.orders
+    assert db_user == db_order.user
